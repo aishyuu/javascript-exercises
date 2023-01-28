acde1a79e73fdafbaf754c2867b31f9da90d7428
aishyuu <ivanmatias1331@gmail.com>
2023-01-28 03:57:57 -0800

    Finished problems 1-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(!(typeof(num1) === "number") || !(typeof(num2) === "number")) {
+        return "ERROR"
+    }
 
+    let min = Math.min(num1, num2);
+    let max = Math.max(num1, num2);
+    let result = 0;
+
+    for(let i = min; i <= max; i++) {
+        result += i;
+    }
+
+    return result
 };
 
 // Do not edit below this line
