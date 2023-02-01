e754d98443e06bd04e1bc9fa37180963042b3753
Ivan Matias <ivanmatias1331@gmail.com>
2023-02-01 14:44:44 -0800

    Finished day 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return(a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return(a - b);
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	const result =  numbers.reduce((end, number) => end + number, 0)
+
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return(a * b);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return(Math.pow(a, b));
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0) {
+    return 1;
+  }
+  let result = 1;
+	for(let i = num; i > 0; i--) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
